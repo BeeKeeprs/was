8439f62adc512d0dad1e0136fbdc2ccbe7b6987b
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-01 16:52:27 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/kr/co/webee/WebeeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.webee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/webee/WebeeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.webee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

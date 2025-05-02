9ee435b5d520809315eccca398a7e67a78140cfc
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-02 18:26:30 +0900

    feat: ProductReview 엔티티 생성

---
+++ src/main/java/kr/co/webee/domain/product/ProductReview.java
@@ -0,0 +1,42 @@
+package kr.co.webee.domain.product;
+
+import jakarta.persistence.*;
+import kr.co.webee.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ProductReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public ProductReview(String content, Product product, User user) {
+        if (!StringUtils.hasText(content)) {
+            throw new IllegalArgumentException("content는 null이거나 빈 문자열이 될 수 없습니다.");
+        }
+
+        this.content = content;
+        this.product = Objects.requireNonNull(product, "product는 null이 될 수 없습니다.");
+        this.user = Objects.requireNonNull(user, "user는 null이 될 수 없습니다.");
+    }
+}

58f4c6cde01bd05dabe039a09438c8c055c5c1c4
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-02 20:29:41 +0900

    feat: BaseTimeEntity 클래스 생성 후 엔티티에 적용

--- src/main/java/kr/co/webee/domain/product/ProductReview.java
+++ src/main/java/kr/co/webee/domain/product/ProductReview.java
@@ -1,6 +1,7 @@
 package kr.co.webee.domain.product;
 
 import jakarta.persistence.*;
+import kr.co.webee.domain.common.BaseTimeEntity;
 import kr.co.webee.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -13,7 +14,7 @@ import java.util.Objects;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class ProductReview {
+public class ProductReview extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

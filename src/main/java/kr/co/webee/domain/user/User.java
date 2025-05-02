71f4dfba63ba5d7b197773174a499738d0b5b14f
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-02 17:45:04 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/kr/co/webee/domain/user/User.java
@@ -0,0 +1,49 @@
+package kr.co.webee.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.util.StringUtils;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ColumnDefault("false")
+    private boolean businessRegistered;
+
+    @Builder
+    public User(String username, String password, String name, boolean businessRegistered) {
+        if (!StringUtils.hasText(username)) {
+            throw new IllegalArgumentException("username은 null이거나 빈 문자열이 될 수 없습니다.");
+        }
+        if (!StringUtils.hasText(password)) {
+            throw new IllegalArgumentException("password는 null이거나 빈 문자열이 될 수 없습니다.");
+        }
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("name은 null이거나 빈 문자열이 될 수 없습니다.");
+        }
+
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.businessRegistered=businessRegistered;
+    }
+}

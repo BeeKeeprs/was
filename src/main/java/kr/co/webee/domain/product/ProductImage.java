76eaf3027839953b4beedfe066c3ef9c2646a12c
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-02 18:16:20 +0900

    feat: ProductImage 엔티티 생성

---
+++ src/main/java/kr/co/webee/domain/product/ProductImage.java
@@ -0,0 +1,37 @@
+package kr.co.webee.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Builder
+    public ProductImage(String imageUrl, Product product) {
+        if (!StringUtils.hasText(imageUrl)) {
+            throw new IllegalArgumentException("imageUrl은 null이거나 빈 문자열이 될 수 없습니다.");
+        }
+
+        this.imageUrl = imageUrl;
+        this.product = Objects.requireNonNull(product, "product는 null이 될 수 없습니다.");
+
+    }
+}

9d7a8a661ed591cd0f110867c40a171a9afdd77a
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-05 20:09:15 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/kr/co/webee/application/user/service/UserService.java
@@ -0,0 +1,20 @@
+package kr.co.webee.application.user.service;
+
+import kr.co.webee.domain.user.entity.User;
+import kr.co.webee.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+}

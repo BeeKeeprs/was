f5aec671363a606d5dcae151f4336051014facc1
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-06 03:34:27 +0900

    feat: cookie 생성 및 삭제 util 클래스

---
+++ src/main/java/kr/co/webee/common/util/cookie/CookieUtil.java
@@ -0,0 +1,23 @@
+package kr.co.webee.common.util.cookie;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+
+public class CookieUtil {
+
+    public static ResponseCookie createCookie(String cookieName, String value, long maxAge) {
+        return ResponseCookie.from(cookieName, value)
+                .path("/")
+                .httpOnly(true)
+                .maxAge(maxAge)
+                .secure(true)
+                .sameSite("None")
+                .build();
+    }
+
+    public static void deleteCookie(String cookieName, HttpServletResponse response) {
+        ResponseCookie cookie = createCookie(cookieName, "", 0);
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+}
+

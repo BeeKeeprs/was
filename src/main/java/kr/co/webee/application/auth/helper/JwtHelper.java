2bb422000189f01d5247a0948e0906cd7b8c32a5
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-06 03:34:52 +0900

    feat: jwt 토큰 관리

---
+++ src/main/java/kr/co/webee/application/auth/helper/JwtHelper.java
@@ -0,0 +1,49 @@
+package kr.co.webee.application.auth.helper;
+
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.webee.application.auth.dto.JwtTokenDto;
+import kr.co.webee.application.auth.service.RefreshTokenService;
+import kr.co.webee.common.auth.JwtProvider;
+import kr.co.webee.common.util.cookie.CookieUtil;
+import kr.co.webee.common.util.jwt.JwtConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtHelper {
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
+
+    public JwtTokenDto createToken(Long userId, String username) {
+        String accessToken = jwtProvider.createAccessToken(username, userId);
+        String refreshToken = jwtProvider.createRefreshToken(username, userId);
+
+        refreshTokenService.save(userId, refreshToken);
+
+        return JwtTokenDto.of(accessToken,refreshToken);
+    }
+
+    public JwtTokenDto reissueToken(String refreshToken) {
+        Long userId = jwtProvider.getUserId(refreshToken);
+
+        if (!refreshTokenService.existsByUserId(userId)) {
+            throw new RuntimeException("리프레시 토큰 존재하지 않는 예외");
+        }
+        refreshTokenService.delete(userId);
+
+        String username = jwtProvider.getUsername(refreshToken);
+        String newAccessToken = jwtProvider.createAccessToken(username, userId);
+        String newRefreshToken = jwtProvider.createRefreshToken(username, userId);
+
+        refreshTokenService.save(userId, newRefreshToken);
+
+        return JwtTokenDto.of(newAccessToken, newRefreshToken);
+    }
+
+    public void deleteRefreshToken(String refreshToken, HttpServletResponse response) {
+        Long userId = jwtProvider.getUserId(refreshToken);
+        CookieUtil.deleteCookie(JwtConstants.REFRESH_TOKEN_COOKIE_KEY, response);
+        refreshTokenService.delete(userId);
+    }
+}

e6957ccaf1eb279a658546c924a7ce71621a3b0a
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-06 03:49:51 +0900

    rename: JwtUtil의 메서드명 변경(parseToken -> resolveToken)

--- src/main/java/kr/co/webee/common/util/jwt/JwtUtil.java
+++ src/main/java/kr/co/webee/common/util/jwt/JwtUtil.java
@@ -3,7 +3,7 @@ package kr.co.webee.common.util.jwt;
 import org.springframework.util.StringUtils;
 
 public class JwtUtil {
-    public static String parseToken(String authHeader) {
+    public static String resolveToken(String authHeader) {
         if (StringUtils.hasText(authHeader) && authHeader.startsWith(JwtConstants.ACCESS_TOKEN_HEADER_PREFIX)) {
             return authHeader.substring(JwtConstants.ACCESS_TOKEN_HEADER_PREFIX.length());
         }

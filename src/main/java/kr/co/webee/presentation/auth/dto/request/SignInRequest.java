51d41fee20f9dfd074e213998788ce10331f0748
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-07 07:00:33 +0900

    test: 로그인 테스트 코드 작성

--- src/main/java/kr/co/webee/presentation/auth/dto/request/SignInRequest.java
+++ src/main/java/kr/co/webee/presentation/auth/dto/request/SignInRequest.java
@@ -2,15 +2,17 @@ package kr.co.webee.presentation.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 @Schema(description = "로그인 request")
 public record SignInRequest(
         @Schema(description = "아이디", example = "exampleId")
-        @NotBlank(message = "아이디는 필수 항목입니다.")
+        @NotBlank
         String username,
 
         @Schema(description = "비밀번호", example = "examplePassword")
-        @NotBlank(message = "비밀번호는 필수 항목입니다.")
+        @NotBlank
         String password
 ) {
 }

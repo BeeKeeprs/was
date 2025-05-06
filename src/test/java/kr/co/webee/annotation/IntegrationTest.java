c3cfd93d9661def51df447a0ec78f36edc3ebba3
이재원 <jaewonlee.pro@gmail.com>
2025-05-06 12:44:45 +0900

    깃허브 액션즈 CI CD 스크립트 추가 (#14)
    * feat: CI CD 스크립트 추가
    
    * fix: 탬플릿 수정
    
    * feat: testcontainers 추가

---
+++ src/test/java/kr/co/webee/annotation/IntegrationTest.java
@@ -0,0 +1,18 @@
+package kr.co.webee.annotation;
+
+import kr.co.webee.config.TestcontainersConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Testcontainers
+@SpringBootTest
+@Import({TestcontainersConfig.class})
+public @interface IntegrationTest {}

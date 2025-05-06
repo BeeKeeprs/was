72c4e584c4b5f62dd4d507913ce2c81dbf1cacf2
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-07 02:35:31 +0900

    docs: auth 관련 swagger 문서화 및 응답 구조 변경

--- src/main/java/kr/co/webee/application/auth/service/AuthService.java
+++ src/main/java/kr/co/webee/application/auth/service/AuthService.java
@@ -1,5 +1,9 @@
 package kr.co.webee.application.auth.service;
 
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.webee.application.auth.dto.JwtTokenDto;
+import kr.co.webee.application.auth.helper.JwtHelper;
+import kr.co.webee.presentation.auth.dto.request.SignInRequest;
 import kr.co.webee.presentation.auth.dto.request.SignUpRequest;
 import kr.co.webee.application.user.service.UserService;
 import kr.co.webee.common.error.ErrorType;
@@ -8,6 +12,7 @@ import kr.co.webee.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -18,21 +23,24 @@ public class AuthService {
 
     public void signup(SignUpRequest request) {
         if (userService.existsByUsername(request.username())) {
-            String message=String.format("username: %s", request.username());
+            String message = String.format("username: %s", request.username());
             throw new BusinessException(ErrorType.ALREADY_EXIST_USERNAME, message);
         }
 
-        String encodedPassword = passwordEncoder.encode(signUpDto.password());
-        User user = signUpDto.toEntity(encodedPassword);
+        String encodedPassword = passwordEncoder.encode(request.password());
+        User user = request.toEntity(encodedPassword);
         userService.save(user);
     }
 
-    public JwtTokenDto signIn(SignInDto signInDto) {
-        User user = userService.readByUsername(signInDto.username())
-                .orElseThrow(() -> new RuntimeException("사용자 존재하지 않음 예외"));
+    public JwtTokenDto signIn(SignInRequest request) {
+        User user = userService.readByUsername(request.username())
+                .orElseThrow(() -> {
+                    String message = String.format("username: %s", request.username());
+                    return new BusinessException(ErrorType.INVALID_CREDENTIALS, message);
+                });
 
-        if (!passwordEncoder.matches(signInDto.password(), user.getPassword())) {
-            throw new RuntimeException("비밀번호 일치하지 않음 예외");
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new BusinessException(ErrorType.FAILED_AUTHENTICATION);
         }
 
         return jwtHelper.createToken(user.getId(), user.getUsername());
--- src/main/java/kr/co/webee/presentation/auth/api/AuthApi.java
+++ src/main/java/kr/co/webee/presentation/auth/api/AuthApi.java
@@ -2,15 +2,32 @@ package kr.co.webee.presentation.auth.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import kr.co.webee.common.util.jwt.JwtConstants;
+import kr.co.webee.presentation.auth.dto.request.SignInRequest;
 import kr.co.webee.presentation.auth.dto.request.SignUpRequest;
 import kr.co.webee.common.error.ErrorType;
 import kr.co.webee.presentation.annotation.ApiDocsErrorType;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Auth")
 public interface AuthApi {
     @Operation(summary = "회원가입", description = "사용자 정보를 등록하는 API")
     @ApiDocsErrorType(ErrorType.ALREADY_EXIST_USERNAME)
-    public void signUp(@RequestBody @Valid SignUpRequest signUpRequest);
+    void signUp(@RequestBody @Valid SignUpRequest signUpRequest);
+
+    @Operation(summary = "로그인", description = "사용자 인증을 하는 API")
+    @ApiDocsErrorType(value = {ErrorType.INVALID_CREDENTIALS, ErrorType.FAILED_AUTHENTICATION})
+    void signIn(@RequestBody @Valid SignInRequest request, HttpServletResponse response);
+
+    @Operation(summary = "access token 재발급", description = "refresh token을 통해 access token을 재발급 받는 API")
+    @ApiDocsErrorType(value = {ErrorType.COOKIE_NOT_FOND, ErrorType.INVALID_ACCESS_TOKEN})
+    void reissueToken(@CookieValue(name = JwtConstants.REFRESH_TOKEN_COOKIE_KEY, required = false) String refreshToken,
+                             HttpServletResponse response);
+
+    @Operation(summary = "로그아웃", description = "사용자의 인증 정보를 삭제하는 API")
+    void signOut(@CookieValue(name = JwtConstants.REFRESH_TOKEN_COOKIE_KEY, required = false) String refreshToken,
+                 HttpServletResponse response);
 }

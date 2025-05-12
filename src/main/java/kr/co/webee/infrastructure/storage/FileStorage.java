7cc93a9a88bfe6f682806c3373747832a1253850
Jaewon Lee <jaewonlee.pro@gmail.com>
2025-05-12 13:08:52 +0900

    feat: fileStorage 추가

---
+++ src/main/java/kr/co/webee/infrastructure/storage/FileStorage.java
@@ -0,0 +1,10 @@
+package kr.co.webee.infrastructure.storage;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorage {
+    /**
+     * 파일을 저장하고 URL 또는 경로를 반환한다.
+     */
+    String upload(MultipartFile file);
+}

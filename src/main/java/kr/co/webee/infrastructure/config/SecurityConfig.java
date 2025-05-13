879df883f9c4378e38d618f6187ae7b1ed4185f1
Jaewon Lee <jaewonlee.pro@gmail.com>
2025-05-13 13:48:49 +0900

    fix: Swagger CORS 에러 수정

--- src/main/java/kr/co/webee/infrastructure/config/SecurityConfig.java
+++ src/main/java/kr/co/webee/infrastructure/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import kr.co.webee.common.auth.filter.JwtAuthenticationEntryPoint;
 import kr.co.webee.common.auth.filter.JwtAuthenticationFilter;
 import kr.co.webee.common.auth.filter.JwtExceptionFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,10 +16,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.stream.Stream;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +37,9 @@ public class SecurityConfig {
     private static final String[] READ_ONLY_ENDPOINTS = {
             "/api/v1/products/**", "/api/v1/users/**"
     };
+
+    @Value("${was.cors-allow-origins}")
+    private List<String> corsOrigins;
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtExceptionFilter jwtExceptionFilter;
@@ -64,5 +73,18 @@ public class SecurityConfig {
 
         return http.build();
     }
-}
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsOrigins);
+        configuration.setAllowedMethods(List.of(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name()));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setMaxAge(3600L); // Cache preflight
+        configuration.setExposedHeaders(List.of(SET_COOKIE, AUTHORIZATION));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}

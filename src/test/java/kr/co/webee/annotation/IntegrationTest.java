3d980f797482c5c0d48bd861c4bba4cc3f17c6df
Jaewon Lee <jaewonlee.pro@gmail.com>
2025-05-14 21:28:05 +0900

    test: LocalStack으로 S3Client 등록하도록 수정

--- src/test/java/kr/co/webee/annotation/IntegrationTest.java
+++ src/test/java/kr/co/webee/annotation/IntegrationTest.java
@@ -1,8 +1,10 @@
 package kr.co.webee.annotation;
 
+import kr.co.webee.config.AwsS3TestConfig;
 import kr.co.webee.config.TestcontainersConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.lang.annotation.ElementType;
@@ -12,7 +14,8 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
 @Testcontainers
 @SpringBootTest
-@Import({TestcontainersConfig.class})
+@Import({TestcontainersConfig.class, AwsS3TestConfig.class})
 public @interface IntegrationTest {}

6af64fadc8db70964388c1259a0b40666f4f73b4
Chaeyoon Kim <cy6kimm@gmail.com>
2025-05-18 18:11:57 +0900

    feat: 수정벌 추천 API

---
+++ src/main/java/kr/co/webee/presentation/bee/recommend/dto/request/UserCropInfoRequest.java
@@ -0,0 +1,40 @@
+package kr.co.webee.presentation.bee.recommend.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import kr.co.webee.domain.profile.crop.type.CultivationType;
+
+import java.time.LocalDate;
+
+@Schema(description = "수정벌 추천 기능, 질병 진단 기능에 활용할 사용자 재배 작물 정보 request")
+public record UserCropInfoRequest(
+        @Schema(description = "작물명", example = "딸기")
+        @NotBlank
+        String name,
+
+        @Schema(description = "품종", example = "설향")
+        @NotBlank
+        String variety,
+
+        @Schema(description = "재배 방식", example = "CONTROLLED", examples = {"CONTROLLED", "OPEN_FIELD"})
+        @NotNull
+        CultivationType cultivationType,
+
+        @Schema(description = "재배 지역", example = "충청남도 논산시 연무읍 봉동리")
+        @NotBlank
+        String cultivationAddress,
+
+        @Schema(description = "재배 면적", example = "1320")
+        @NotNull
+        Integer cultivationArea,
+
+        @Schema(description = "정식(또는 파종)일", example = "2024-02-25")
+        @NotNull
+        LocalDate plantingDate
+) {
+    public String describe() {
+        return String.format("작물명: %s, 품종: %s, 재배 방식: %s, 재배 지역: %s, 재배 면적: %s, 정식(또는 파종)일: %s",
+                this.name, this.variety, this.cultivationType.getDescription(), this.cultivationAddress, this.cultivationArea, this.plantingDate.toString());
+    }
+}

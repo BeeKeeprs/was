d9f47334d1262b9201816b233c6761b05cad2ac9
Chaeyoon Kim <cy6kimm@gmail.com>
2026-05-28 03:18:51 +0900

    feat: 벌통 연결 상태 조회 API

--- src/main/java/kr/co/webee/application/hive/service/HiveConnectionService.java
+++ src/main/java/kr/co/webee/application/hive/service/HiveConnectionService.java
@@ -1,6 +1,9 @@
 package kr.co.webee.application.hive.service;
 
 import kr.co.webee.application.hive.dto.request.HiveConnectionRequest;
+import kr.co.webee.application.hive.dto.response.HiveConnectionResponse;
+import kr.co.webee.common.error.ErrorType;
+import kr.co.webee.common.error.exception.BusinessException;
 import kr.co.webee.domain.hive.entity.Hive;
 import kr.co.webee.domain.hive.repository.HiveRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +21,13 @@ public class HiveConnectionService {
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 벌통입니다. macAddress=" + macAddress));
 
         hive.updateConnection(request.isConnected());
+    }
+
+    @Transactional(readOnly = true)
+    public HiveConnectionResponse getConnection(Long hiveId, Long userId) {
+        Hive hive = hiveRepository.findByIdAndUserId(hiveId, userId)
+                .orElseThrow(() -> new BusinessException(ErrorType.HIVE_NOT_FOUND));
+
+        return HiveConnectionResponse.from(hive);
     }
 }

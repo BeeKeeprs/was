d2750c0e581ffabc114b45da687bf2f9e0d5ba25
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-11 14:35:57 +0900

    chore: fmc 설정

---
+++ src/main/java/kr/co/webee/infrastructure/fcm/FcmConfig.java
@@ -0,0 +1,31 @@
+package kr.co.webee.infrastructure.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@Configuration
+public class FcmConfig {
+    @Value("${firebase.key-path}")
+    private String firebaseKeyPath;
+
+    @Bean
+    public FirebaseApp initializeFirebase() throws IOException {
+        InputStream serviceAccount = new ClassPathResource(firebaseKeyPath).getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}

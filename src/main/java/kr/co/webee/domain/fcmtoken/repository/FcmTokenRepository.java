eb7c600f80379729a85e9a3b875317f7521b92b5
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-11 15:54:56 +0900

    feat: 회원 탈퇴 시 FCM Token 삭제 로직 추가

--- src/main/java/kr/co/webee/domain/fcmtoken/repository/FcmTokenRepository.java
+++ src/main/java/kr/co/webee/domain/fcmtoken/repository/FcmTokenRepository.java
@@ -1,6 +1,7 @@
 package kr.co.webee.domain.fcmtoken.repository;
 
 import kr.co.webee.domain.fcmtoken.entity.FcmToken;
+import kr.co.webee.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     Optional<FcmToken> findByUserIdAndDeviceInfo(Long userId, String deviceInfo);
 
     void deleteByUserIdAndDeviceInfo(Long userId, String deviceInfo);
+
+    void deleteAllByUserId(Long userId);
 }

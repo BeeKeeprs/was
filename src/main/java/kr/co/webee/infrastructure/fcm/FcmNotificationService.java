fc68d8992f40103a91fee4e17cf3963e727d4e25
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-11 14:47:52 +0900

    feat: fcm 알림 발송 로직 구현

---
+++ src/main/java/kr/co/webee/infrastructure/fcm/FcmNotificationService.java
@@ -0,0 +1,33 @@
+package kr.co.webee.infrastructure.fcm;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class FcmNotificationService {
+
+    public void send(String token, String title, String content) {
+        Message message = buildMessage(token, title, content);
+
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+        } catch (FirebaseMessagingException e) {
+            log.error("FCM 알림 전송 실패. {}", e.getMessage());
+        }
+    }
+
+    private Message buildMessage(String token, String title, String content) {
+        return Message.builder()
+                .setToken(token)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(content)
+                        .build())
+                .build();
+    }
+}

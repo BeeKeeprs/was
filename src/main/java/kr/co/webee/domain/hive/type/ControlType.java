f2e5dd4d77352c68100ec153f1fc92559a1cba00
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-20 17:17:35 +0900

    feat: 제어 타입 재정의

--- src/main/java/kr/co/webee/domain/hive/type/ControlType.java
+++ src/main/java/kr/co/webee/domain/hive/type/ControlType.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -12,18 +11,18 @@ import java.util.List;
 public enum ControlType {
     TEMPERATURE("온도"),
     HUMIDITY("습도"),
-    CO2("CO2"),
     FAN("팬"),
     HEATER("히터"),
-    DOOR("문");
+    COOLER("냉각기"),
+    CIRCULATION("환기");
 
     private final String description;
 
     public static List<ControlType> autoControlTypes() {
-        return Arrays.asList(values());
+        return List.of(TEMPERATURE, HUMIDITY, FAN);
     }
 
     public static List<ControlType> manualControlTypes() {
-        return List.of(FAN, HEATER, DOOR);
+        return List.of(HEATER, COOLER, FAN, CIRCULATION);
     }
 }

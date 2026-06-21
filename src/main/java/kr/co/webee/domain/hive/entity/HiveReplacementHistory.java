c5e5b33b79d453442698f068a0fae0952e785d6a
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-21 23:39:44 +0900

    feat: 벌통 교체 기록 엔티티 생성

---
+++ src/main/java/kr/co/webee/domain/hive/entity/HiveReplacementHistory.java
@@ -0,0 +1,33 @@
+package kr.co.webee.domain.hive.entity;
+
+import jakarta.persistence.*;
+import kr.co.webee.domain.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class HiveReplacementHistory extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Hive hive;
+
+    @Column(nullable = false)
+    private LocalDate replacedAt;
+
+    @Builder
+    private HiveReplacementHistory(Hive hive, LocalDate replacedAt) {
+        this.hive = Objects.requireNonNull(hive, "hive는 null이 될 수 없습니다.");
+        this.replacedAt = Objects.requireNonNull(replacedAt, "replacedAt은 null이 될 수 없습니다.");
+    }
+}

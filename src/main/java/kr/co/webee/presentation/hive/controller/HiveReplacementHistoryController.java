913a95997e1382eace2f91e93096319755c7fd78
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-22 00:47:53 +0900

    feat: 벌통 교체 기록 목록 조회 API

--- src/main/java/kr/co/webee/presentation/hive/controller/HiveReplacementHistoryController.java
+++ src/main/java/kr/co/webee/presentation/hive/controller/HiveReplacementHistoryController.java
@@ -5,8 +5,14 @@ import kr.co.webee.application.hive.service.HiveReplacementHistoryService;
 import kr.co.webee.presentation.hive.api.HiveReplacementHistoryApi;
 import kr.co.webee.presentation.hive.dto.request.HiveReplacementHistoryCreateRequest;
 import kr.co.webee.presentation.hive.dto.response.HiveReplacementHistoryCreateResponse;
+import kr.co.webee.presentation.hive.dto.response.HiveReplacementHistoryListResponse;
 import kr.co.webee.presentation.support.annotation.UserId;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +33,15 @@ public class HiveReplacementHistoryController implements HiveReplacementHistoryA
             @RequestBody @Valid HiveReplacementHistoryCreateRequest request
     ) {
         return hiveReplacementHistoryService.createReplacementHistory(hiveId, userId, request);
+    }
+
+    @Override
+    @GetMapping("/{hiveId}/replacement-history")
+    public Slice<HiveReplacementHistoryListResponse> getAllReplacementHistories(
+            @PathVariable Long hiveId,
+            @UserId Long userId,
+            @PageableDefault(size = 10, sort = "replacedAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        return hiveReplacementHistoryService.getAllReplacementHistories(hiveId, userId, pageable);
     }
 }

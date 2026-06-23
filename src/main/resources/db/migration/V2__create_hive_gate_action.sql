56cecc9ddb8a5ada667a357213543f375c59f443
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-23 11:40:52 +0900

    feat: hive_gate_action 테이블 추가에 따른 flyway 스크립트 작성

---
+++ src/main/resources/db/migration/V2__create_hive_gate_action.sql
@@ -0,0 +1,13 @@
+CREATE TABLE hive_gate_action
+(
+    id             BIGINT       NOT NULL AUTO_INCREMENT,
+    hive_id        BIGINT       NOT NULL,
+    title          VARCHAR(255) NOT NULL,
+    action_type    VARCHAR(20)  NOT NULL,
+    action_time    TIME         NOT NULL,
+    repeat_enabled TINYINT(1)   NOT NULL DEFAULT 0,
+    created_at     DATETIME(6)  NOT NULL,
+    modified_at    DATETIME(6),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_hive_gate_action_hive FOREIGN KEY (hive_id) REFERENCES hive (id)
+);

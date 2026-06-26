ba3b75883ccc5ef66d0e170693e9ed68c6eb6697
Chaeyoon Kim <cy6kimm@gmail.com>
2026-06-26 15:02:57 +0900

    feat: 뉴스 수집 서비스 및 스케줄러

--- src/main/java/kr/co/webee/domain/interestnewskeyword/repository/InterestNewsKeywordRepository.java
+++ src/main/java/kr/co/webee/domain/interestnewskeyword/repository/InterestNewsKeywordRepository.java
@@ -2,6 +2,7 @@ package kr.co.webee.domain.interestnewskeyword.repository;
 
 import kr.co.webee.domain.interestnewskeyword.entity.InterestNewsKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface InterestNewsKeywordRepository extends JpaRepository<InterestNewsKeyword, Long> {
     List<InterestNewsKeyword> findByUserId(Long userId);
+
+    @Query("SELECT DISTINCT k.keyword FROM InterestNewsKeyword k")
+    List<String> findAllDistinctKeywords();
 }
 
---
+++ src/main/java/kr/co/webee/infrastructure/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package kr.co.webee.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+@Profile("!test")
+public class SchedulingConfig {
+}

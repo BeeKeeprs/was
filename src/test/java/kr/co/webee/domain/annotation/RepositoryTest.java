89dd2928bcc93e3e4d7ce03229944c03977c5e24
Chaeyoon Kim <cy6kimm@gmail.com>
2026-09-21 01:04:14 +0900

    Revert "fix: @RepositoryTest에 test 프로파일 활성화 추가"
    This reverts commit 1c0d6cd7f4c3cbeb49acadc970e082892741041e.

--- src/test/java/kr/co/webee/domain/annotation/RepositoryTest.java
+++ src/test/java/kr/co/webee/domain/annotation/RepositoryTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import org.springframework.test.context.ActiveProfiles;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -16,7 +14,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ActiveProfiles("test")
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
